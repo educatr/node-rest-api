69c33d85886e483a340e355e171ce3fcb71312e7
educationcomoros@gmail.com <kaissouneali1@gmail.com>
2023-09-25 18:02:23 +0300

    project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose =require("mongoose");
 const heltmet=require("helmet");
 const morgan =require("morgan");
+const bodyParser = require('body-parser');
 const dotenv=require("dotenv");
 dotenv.config();
 const userRoute=require("./routes/users")
@@ -10,11 +11,12 @@ const authRoute=require("./routes/auth");
 const app = express();
 //Express middleware
 express(express.json());
+app.use(bodyParser.json());
 app.use(heltmet());
 app.use(morgan("common"));
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
-
+ 
 try {
   mongoose.connect(process.env.MONGODB_URL,
     console.log("mongodb is connected ❤️"))
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,36 @@
 const router=require("express").Router();
+const { response } = require("express");
 const User =require("../models/User");
-
-router.get("/register", async (req,res) =>{
-    // i use get to get the information on the browser before using the post to create 
-    // create new user 
-    const user=  new User({
-      username:"john",
-      email:"jen@gmail.com",
-      password:"123457"
-    });
-    await user.save();
-   res.send("ok")
+const bcript =require("bcrypt");
+router.post("/register", async (req,res) =>{
+    try {
+      //Hashing the Password
+        const salt= await bcript.genSalt(10);
+        const hashedPassword =await bcript.hash(req.body.password, salt);
+      //create new user 
+     const newUser=  new User({
+     username:req.body.username,
+     email:req.body.email,
+     password:hashedPassword
+});
+//     save user 
+        const user=await newUser.save();
+        // return the reponse
+        res.status(201).json(user);
+    } catch (error) {
+      console.log("error occured coz",error)
+    }
 });
 
-module.exports = router;
+//Login is meant find user if exist
 
+router.post("/login",async(req,res)=>{
+  try {
+    const user =await User.findOne({email:req.body.email});
+  !user && res.status(404).send("user is not found")
+  } catch (error) {
+  
+    console.log(error)
+  }
+})
+module.exports = router
